8d6c47f3f249b3d75df752285745bf2ae3024c11
omar96developer <omakino@hotmail.it>
2021-12-28 02:41:39 +0100

    complete with bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    @dump($movies)
-    @foreach ($movies as $movie)
-        <h1>{{$movie->title}}</h1>
-        <h3>{{$movie->original_title}}</h3>
-        <h3>{{$movie->nationality}}</h3>
-    @endforeach
+    <main>
+        @foreach ($movies as $key=>$movie)
+        <div class="card">
+            <h1>{{$movie->title}}</h1>
+            <h3>Original Title: {{$movie->original_title}}</h3>
+            <h3>Nationality: {{$movie->nationality}}</h3>
+            <h4>Date Of Publication: {{$movie->date}}</h4>
+            <h4>Vote: {{$movie->vote}}</h4>
+        </div>
+        
+        @endforeach
+    </main>
+    
 </body>
 </html>
